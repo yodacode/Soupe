e59ada8396d10d949adab8df26efb02e8f7c64c4
yodacode <benjamin.devaublanc@gmail.com>
2014-05-01 20:04:33 +0200

    add bootstrap

---
+++ app/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>App : Listing</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    
+  </head>
+  <body>
+
+  	<div class="container">
+    	<h1>Hello, world!</h1>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="jquery/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/App.js"></script>
+  </body>
+</html>
