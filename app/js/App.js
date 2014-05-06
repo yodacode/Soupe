84579365a118b317872a5207d89055d4ba24defe
yodacode <benjamin.devaublanc@gmail.com>
2014-05-06 15:29:21 +0200

    upgrade js

--- app/js/App.js
+++ app/js/App.js
@@ -12,50 +12,42 @@ $(function () {
 			var that = this;
 			this.UI.selectTwons = $('#townsSelect');
 			this.UI.selectCountries = $('#countriesSelect');
-			this.UI.selectAddPlace = $('#selectAddPlace');		
+			this.UI.selectAddPlace = $('#selectAddPlace');
 
 			this.getCountries(function (xml) {
-				var option = $('<option>').appendTo(that.UI.selectCountries);
-				xml.find('item').each(function () {
-					var option = $('<option>')
-						.attr({value: $(this).find('id').text()})
-						.text($(this).find('name').text())
-						.appendTo(that.UI.selectCountries);
-				});
+				that.createSelect(xml.find('item'), that.UI.selectCountries);
 			});
 
-			this.getTowns('', function (xml) {				
-				xml.find('item').each(function () {
-					var option = $('<option>')
-						.attr({value: $(this).find('id').text()})
-						.text($(this).find('name').text())
-						.appendTo(that.UI.selectAddPlace);
-				});
+			this.getTowns('', function (xml) {
+				that.createSelect(xml.find('item'), that.UI.selectAddPlace);
 			});
 		},
 		bind: function () {
 			var that = this;
 
-			this.UI.selectCountries.on('change', function () {	
-
-				that.getTowns($(this).val(), function (xml) {				
-					that.UI.selectTwons.empty();
-					var option = $('<option>').appendTo(that.UI.selectTwons);
-					xml.find('item').each(function () {
-						var option = $('<option>')
-							.attr({value: $(this).find('id').text()})
-							.text($(this).find('name').text())
-							.appendTo(that.UI.selectTwons);
-					});
+			this.UI.selectCountries.on('change', function () {
+				that.getTowns($(this).val(), function (xml) {
+					that.createSelect(xml.find('item'), that.UI.selectTwons);
 				});
 			})
-		},		
+		},
+		createSelect: function (items, select) {
+
+			select.empty();
+			$('<option>').appendTo(select);
+			items.each(function () {
+				var option = $('<option>')
+					.attr({value: $(this).find('id').text()})
+					.text($(this).find('name').text())
+					.appendTo(select);
+			});
+		},
 		getTowns: function (countryId, callback) {
 			$.ajax({
 				url: 'http://rest.dev/app/get-towns.php?country_id=' + countryId,
 				type: 'GET',
 				dataType: "xml",
-				complete: function (data) {				
+				complete: function (data) {
 					var xml = $(data.responseXML);
 					callback(xml);
 				}
@@ -66,7 +58,7 @@ $(function () {
 				url: 'http://rest.dev/app/get-countries.php',
 				type: 'GET',
 				dataType: "xml",
-				complete: function (data) {				
+				complete: function (data) {
 					var xml = $(data.responseXML);
 					callback(xml);
 				}
